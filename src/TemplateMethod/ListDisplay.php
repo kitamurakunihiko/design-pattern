d522f704cb13379e497d40b3ef94ee05ee7d6c0d
kitamurakunihiko <fourthkwitht_9292@yahoo.co.jp>
2022-11-28 17:39:13 +0900

    ListDisplayクラス追加

--- src/TemplateMethod/ListDisplay.php
+++ src/TemplateMethod/ListDisplay.php
@@ -2,7 +2,24 @@
 
 namespace Kitamurakunihiko\DesignPattern\TemplateMethod;
 
-class ListDisplay
+class ListDisplay extends AbstractDisplay
 {
 
+    protected function displayHeader()
+    {
+        echo '<dl>';
+    }
+
+    protected function displayBody()
+    {
+        foreach ($this->getData() as $key => $value) {
+            echo '<dt>Item ' . $key . '</dt>dl>';
+            echo '<dd>' . $value . '</dd>';
+        };
+    }
+
+    protected function displayFooter()
+    {
+        echo '</dl>';
+    }
 }
