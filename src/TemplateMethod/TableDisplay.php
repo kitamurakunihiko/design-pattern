8989b263e94afd309ca7c6c9f495ae0780f582e2
kitamurakunihiko <fourthkwitht_9292@yahoo.co.jp>
2022-11-28 17:49:44 +0900

    TableDisplayクラスの追加

---
+++ src/TemplateMethod/TableDisplay.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kitamurakunihiko\DesignPattern\TemplateMethod;
+
+class TableDisplay extends AbstractDisplay
+{
+
+    protected function displayHeader()
+    {
+        echo '<table>';
+    }
+
+    protected function displayBody()
+    {
+        foreach ($this->getData() as $key => $value) {
+            echo '<tr>';
+            echo '<th>' .$key . '</th>';
+            echo '<td>' .$value . '</td>';
+            echo '</tr>';
+        }
+    }
+
+    protected function displayFooter()
+    {
+        echo '</table>';
+    }
+}
